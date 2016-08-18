0d2e0fc5e99da18f2d88e386fbcb027186ec8923
Rabea Gleissner <rabeagleissner@gmail.com>
2016-08-18 10:36:48 +0100

    adjustments to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,22 @@
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
-function addToCache (cacheKey, request, response) {
-  if (response.ok) {
-    var copy = response.clone();
-    caches.open(cacheKey).then( cache => {
-      cache.put(request, copy);
-    });
-  }
-  return response;
-}
-
-function fetchFromCache (event) {
-  return caches.match(event.request).then(response => {
-    if (!response) {
-      throw Error(`${event.request.url} not found in cache`);
-    }
-    return response;
-  });
-}
-
-self.addEventListener('install', event => {
-  function onInstall (event) {
+function cacheAssets(event) {
     return caches.open(PRECACHE)
-      .then(cache => cache.addAll([
+        .then(cache => cache.addAll([
                     'styles.css',
                     'script.js',
                     'home.html'
-            ]));
-  }
+        ]));
+}
 
+self.addEventListener('install', event => {
   event.waitUntil(
-    onInstall(event).then( () => self.skipWaiting() )
+    cacheAssets(event).then( () => self.skipWaiting() )
   );
 });
 
-function onActivate(event) {
+function clearOldCaches(event) {
     var currentCaches = [PRECACHE, RUNTIME];
     return caches.keys()
         .then(cacheNames => {
@@ -47,31 +28,28 @@ function onActivate(event) {
 
 self.addEventListener('activate', event => {
     event.waitUntil(
-            onActivate(event)
+            clearOldCaches(event)
             .then(() => self.clients.claim())
             );
 });
 
 self.addEventListener('fetch', event => {
-    console.log('fetch event triggered');
     var url = new URL(event.request.url)
 
         if (url.origin === self.location.origin) {
             event.respondWith(
-                    caches.match(event.request).then(cachedResponse => {
-                        if (cachedResponse) {
-                            return cachedResponse;
-                        }
-                        return caches.open(RUNTIME).then(cache => {
-                            return fetch(event.request)
-                                .then(response => {
-                                    return cache.put(event.request, response.clone())
-                                        .then(() => {
-                                            return response;
-                                        });
-                                });
+                caches.match(event.request).then(cachedResponse => {
+                    if (cachedResponse) {
+                        return cachedResponse;
+                    }
+                    return caches.open(RUNTIME).then(cache => {
+                        return fetch(event.request).then(response => {
+                            return cache.put(event.request, response.clone()).then(() => {
+                                return response;
+                            });
                         });
-                    })
-                    )
+                    });
+                })
+            )
         }
 });
